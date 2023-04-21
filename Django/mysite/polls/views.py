1461b8ab5ec936b6680c2ead8e7ba14ccc72bb35
meliaramendoza <meliaramendoza@gmail.com>
2023-04-21 18:50:29 -0400

    Parte4

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
 from .models import Choice, Question
-
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
