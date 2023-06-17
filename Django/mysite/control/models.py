be8f99c25268933ecc5d51361aa5ff528e34e0bc
meliaramendoza <meliaramendoza@gmail.com>
2023-06-16 21:19:13 -0400

    P2 - Creation of models

--- Django/mysite/control/models.py
+++ Django/mysite/control/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Empleado(models.Model):
+    TURNOS = [
+        ('Día', 'Día'),
+        ('Tarde', 'Tarde'),
+        ('Noche', 'Noche'),
+    ]
+
+    identificador = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150, unique=True)
+    departamento = models.CharField(max_length=100)
+    fecha_inicio = models.DateField(auto_now_add=True)
+    dias_trabajo = models.CharField(max_length=50)
+    turno = models.CharField(max_length=10, choices=TURNOS)
+    horario_entrada = models.TimeField()
+    horario_salida = models.TimeField()
+
+    def __str__(self):
+        return self.nombre
+
+class Jornada(models.Model):
+    TIPOS_MARCACION = [
+        ('entrada', 'Entrada'),
+        ('salida', 'Salida'),
+    ]
+
+    identificador = models.AutoField(primary_key=True)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    tipo_marcacion = models.CharField(max_length=10, choices=TIPOS_MARCACION)
+
+    def __str__(self):
+        return f'Jornada {self.identificador} - Empleado: {self.empleado.nombre}'
